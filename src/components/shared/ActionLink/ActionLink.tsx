bde46404e6e58ccac9a8271be513c3c79c46fb67
Vlad <vlasisla1988@gmail.com>
2024-06-08 22:17:47 +0300

    feature/header: Added action link component

---
+++ src/components/shared/ActionLink/ActionLink.tsx
@@ -0,0 +1,45 @@
+import { ButtonType } from '@/types/generalTypes';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { LINK_STYLES } from './ActionLink.utils';
+
+interface ActionLinkProps {
+  text: string;
+  linkType: ButtonType;
+  href: string;
+  className?: string;
+  icon?: React.ReactSVGElement;
+  isDisabled?: boolean;
+}
+
+export const ActionLink: React.FC<ActionLinkProps> = ({
+  text,
+  linkType,
+  href,
+  className,
+  icon,
+  isDisabled,
+}) => {
+  const linkClasses = classNames(
+    className,
+    LINK_STYLES.base,
+    LINK_STYLES[linkType],
+    isDisabled && LINK_STYLES.disabled,
+  );
+
+  if (isDisabled) {
+    return (
+      <div className={linkClasses}>
+        {icon && icon}
+        <span>{text}</span>
+      </div>
+    );
+  }
+
+  return (
+    <Link href={href} className={linkClasses}>
+      {icon && icon}
+      <span>{text}</span>
+    </Link>
+  );
+};
--- src/components/shared/Navigation/Navigation.tsx
+++ src/components/shared/Navigation/Navigation.tsx
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 import { naviationLinks } from '@/constants/generalConstants';
 import { NavigationLink } from '../NavigationLink/NavigationLink';
 import { usePathname } from 'next/navigation';
-import { ActionButton } from '../ActionButton/ActionButton';
 
 interface NavigationProps {
   className?: string;
@@ -20,9 +19,6 @@ export const Navigation: React.FC<NavigationProps> = ({ className }) => {
         {naviationLinks.map((link) => (
           <NavigationLink key={link.path} isActive={path === link.path} {...link} />
         ))}
-        <li>
-          <ActionButton text="Login" onClick={() => console.log('Test')} buttonType="secondary" />
-        </li>
       </ul>
     </nav>
   );
