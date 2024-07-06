9f39fcbaac013efaeba1892b7fa49fd8084d1168
Vlad <vlasisla1988@gmail.com>
2024-07-06 21:31:29 +0300

    feature/signin-page: Added form components with styles

---
+++ src/components/shared/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+import { HidePasswordButton } from './components/HidePasswordButton';
+
+interface PasswordInputProps {
+  placeholder: string;
+  register: object;
+  inputClasses?: string;
+  labelClasses?: string;
+  isDisabled?: boolean;
+}
+
+export const PasswordInput: React.FC<PasswordInputProps> = ({
+  placeholder,
+  inputClasses,
+  labelClasses,
+  register,
+  isDisabled,
+}) => {
+  const [isPassworsVisible, setIsPasswordVisible] = useState(false);
+  const labelClassList = classNames('block relative overflow-hidden', labelClasses);
+  const inputClassList = classNames(
+    'block  bg-white w-full border-2 border-slate-500 focus:border-blue-500 focus:outline-none rounded-[7px] p-[5px] pr-[70px]',
+    inputClasses,
+  );
+  const inputType = isPassworsVisible ? 'text' : 'password';
+  const buttonClickHandler = () => setIsPasswordVisible((prevValue) => !prevValue);
+
+  return (
+    <label className={labelClassList}>
+      <input
+        className={inputClassList}
+        type={inputType}
+        placeholder={placeholder}
+        disabled={isDisabled}
+        {...register}
+      />
+      <HidePasswordButton isPassworsVisible={isPassworsVisible} onClick={buttonClickHandler} />
+    </label>
+  );
+};
--- src/components/shared/SignInForm/SignInForm.tsx
+++ src/components/shared/SignInForm/SignInForm.tsx
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { ActionButton } from '../ActionButton/ActionButton';
 import { ButtonType } from '@/constants/generalConstants';
+import { TextInput } from '../TextInput/TextInput';
+import { FormWrapper } from '../FormWrapper/FormWrapper';
+import { PasswordInput } from '../PasswordInput/PasswordInput';
 
 interface SignInFormProps {
   className?: string;
@@ -13,26 +16,50 @@ interface SigninFormInputs {
   name: string;
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
 export const SignInForm: React.FC<SignInFormProps> = ({ className }) => {
-  const { register, handleSubmit, reset } = useForm<SigninFormInputs>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    // formState: { errors, isSubmitting },
+  } = useForm<SigninFormInputs>();
   const onSubmit: SubmitHandler<SigninFormInputs> = (data) => {
     console.info(data);
     reset();
   };
-  const formClasses = classNames('w-full', className);
+  const formClasses = classNames('w-full p-[30px]', className);
   return (
-    <form className={formClasses} onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" {...register('name')} />
-      <input type="email" {...register('email')} />
-      <input type="password" {...register('password')} />
+    <FormWrapper className={formClasses} onSubmit={handleSubmit(onSubmit)}>
+      <TextInput
+        labelClasses="mb-[20px] mx-auto w-2/3"
+        placeholder="Name"
+        register={{ ...register('name') }}
+      />
+      <TextInput
+        labelClasses="mb-[20px] mx-auto w-2/3"
+        placeholder="Email"
+        register={{ ...register('email') }}
+      />
+      <PasswordInput
+        labelClasses="mb-[20px] mx-auto w-2/3"
+        placeholder="Password"
+        register={{ ...register('password') }}
+      />
+      <PasswordInput
+        labelClasses="mb-[20px] mx-auto w-2/3"
+        placeholder="Confirm password"
+        register={{ ...register('confirmPassword') }}
+      />
       <ActionButton
         onClick={() => {}}
         buttonType={ButtonType.PRIMARY}
         type="submit"
         text="Sign in"
+        className="mx-auto"
       />
-    </form>
+    </FormWrapper>
   );
 };
